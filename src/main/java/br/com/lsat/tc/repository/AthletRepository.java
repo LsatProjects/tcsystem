c080ba5f44abb9bbc47847fe421a60499f21ff28
fernando.meireles <fernando.meireles@mirante.net.br>
2016-03-23 16:07:19 -0300

    Config of persistence layer and Angular2.

--- src/main/java/br/com/lsat/tc/repository/AthletRepository.java
+++ src/main/java/br/com/lsat/tc/repository/AthletRepository.java
@@ -3,7 +3,9 @@ package br.com.lsat.tc.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.lsat.tc.entity.Athlet;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AthletRepository extends JpaRepository<Athlet, Long> {
 
 }
